0c337b770dd9bb5cbe197d36dfd909999acf3f38
Prakhar Sharma <40967818+Neil3108@users.noreply.github.com>
2023-06-19 19:53:03 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'https://technotes.onrender.com',
-    'https://technotes-vypq.onrender.com'
+    'https://technotes-api.onrender.com',
+    'https://technotes-vypq.onrender.com',
+    'https://technotes-api.onrender.com/auth'
 ]
 
 module.exports = allowedOrigins
